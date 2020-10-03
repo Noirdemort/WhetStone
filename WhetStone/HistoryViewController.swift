cad2866251a6db98a37c8675f5974ea8c3b7ad06
noirdemort <darkStillwater@protonmail.com>
2020-10-03 11:58:46 +0530

    Add file storage removal and cache updation

--- WhetStone/HistoryViewController.swift
+++ WhetStone/HistoryViewController.swift
@@ -13,19 +13,31 @@ class HistoryViewController: UIViewController {
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+		historyTableView.delegate = self
+		historyTableView.dataSource = self
 		if let stored = UserDefaults.standard.array(forKey: "accessCodes") as? [String] {
+			print(history)
 			history = stored
 		}
+		print(history)
     }
     
 	var selectedHistory = 0
+	
+	@IBOutlet weak var historyTableView: UITableView!
 
+	
+	@IBAction func goBack(_ sender: Any){
+		self.navigationController?.popViewController(animated: true)
+	}
     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		
 		if let destination = segue.destination as? SummaryViewController {
+			UserDefaults.standard.set(history, forKey: "accessCodes")
 			var localParser = CSVParser()
 			localParser.parse(rawData: history[self.selectedHistory])
 			destination.parser = localParser
@@ -37,6 +49,34 @@ class HistoryViewController: UIViewController {
 
 
 extension HistoryViewController: UITableViewDelegate {
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+			
+		if editingStyle == .delete {
+			
+			// remove corresponding file
+			do {
+				let fileManager = FileManager.default
+				let docs = try fileManager.url(for: .documentDirectory,
+											   in: .userDomainMask,
+											appropriateFor: nil, create: false)
+				let path = docs.appendingPathComponent(history[indexPath.row])
+			  
+				try fileManager.removeItem(atPath: path.absoluteString)
+				
+			} catch {
+			  // handle error
+			}
+
+			// remove the item from the data model
+			self.history.remove(at: indexPath.row)
+				
+			// delete the table view row
+			self.historyTableView.deleteRows(at: [indexPath], with: .fade)
+			
+			
+		}
+	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		self.selectedHistory = indexPath.row
